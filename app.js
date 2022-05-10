f6f1a40159e9ca5f9a3277276b2ed8f12da1d3d4
muradctg <wri_murad@yahoo.com>
2022-05-09 20:23:40 -0400

    Used Express-FileUpload Package to set up the path and also used CLoudinary

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+  cloud_name:process.env.CLOUD_NAME,
+  api_key:process.env.CLOUD_API_KEY,
+  api_secret:process.env.CLOUD_API_SECRET,
+})
+
 // database
 const connectDB = require('./db/connect');
 
@@ -12,6 +20,10 @@ const productRouter = require('./routes/productRoutes')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(fileUpload({useTempFiles:true}))
 
 app.get('/', (req, res) => {
   res.send('<h1>File Upload STARTER</h1>');
@@ -22,6 +34,8 @@ app.use('/api/v1/products', productRouter)
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
+
 
 const port = process.env.PORT || 3000;
 
